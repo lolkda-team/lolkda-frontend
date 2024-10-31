25744ce8585f8d994501491f20bac998b5b26407
devwoodie <ehddl453@naver.com>
2024-10-31 16:00:39 +0900

    feat: 언어 변경 ui 추가

--- src/components/atoms/LanguageButton.tsx
+++ src/components/atoms/LanguageButton.tsx
@@ -1,20 +1,49 @@
+import React from 'react';
 import styled from '@emotion/styled';
 import LanguageIcon from '@mui/icons-material/Language';
 import { useTheme } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export const LanguageButton = () => {
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return(
-    <S.Stack>
-      <LanguageIcon style={{fontSize: '32px', color: theme.palette.text.secondary}} />
-    </S.Stack>
+    <S.Wrapper>
+      <S.Stack onClick={handleClick}>
+        <LanguageIcon style={{fontSize: '32px', color: theme.palette.text.secondary}} />
+      </S.Stack>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Korean</MenuItem>
+        <MenuItem onClick={handleClose}>English</MenuItem>
+      </Menu>
+    </S.Wrapper>
   )
 }
 
 const S = {
-  Stack: styled.span`
+  Wrapper: styled.div`
     cursor: pointer;
     padding: 5px;
     margin-left: 10px;
+  `,
+  Stack: styled.span`
   `,
 };
